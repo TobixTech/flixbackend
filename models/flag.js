3d612251d5ffdd5f3b61fb5cec04d85349b700dc
TobixTech <tobixtech@gmail.com>
2025-09-06 04:01:59 +0100

    Update flag.js

--- models/flag.js
+++ models/flag.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const flagSchema = new mongoose.Schema({
+  confession_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Confession', required: true },
+  reason: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
+  resolved: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model('Flag', flagSchema);
